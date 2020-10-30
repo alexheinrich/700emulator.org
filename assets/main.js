d9dd09400eae1c7362db8838292798170b64501b
Alexander Heinrich <alex.heinrich@icloud.com>
2020-10-30 15:38:41 +0100

    Mouse follower text with front matter.

--- assets/main.js
+++ assets/main.js
@@ -7,18 +7,16 @@ window.oncontextmenu = function goBack(e) {
 
 const follower = document.querySelector('.mouseFollower');
 
-const typed = new Typed('.mouseFollower', {
-  strings: [
-    'Welcome to the page of the Buchla 700 emulator.',
-    // 'It mimics the operating system of the 700.',
-    // 'This is the main menu. Click on one of items to get more info.',
-  ],
-  typeSpeed: 50,
-  showCursor: false,
-  fadeOut: true,
-  onComplete: (self) => {
-  },
-});
+try {
+  const typed = new Typed('.mouseFollower', {
+    strings: JSON.parse(document.querySelector('.mouseFollowerJson').innerHTML),
+    typeSpeed: 50,
+    showCursor: false,
+    fadeOut: true,
+    onComplete: (self) => {
+    },
+  });
+} catch (e) {}
 
 const setFollower = (e) => {
   follower.style.top = (e.pageY + 40) + 'px';
