b236fa64f224624447606988cd97c240a218b6dc
Alexander Heinrich <alex.heinrich@icloud.com>
2020-11-03 23:09:48 +0100

    Add basic draw on canvas.

--- assets/main.js
+++ assets/main.js
@@ -16,12 +16,13 @@ try {
     onComplete: (self) => {
     },
   });
-} catch (e) {}
 
-const setFollower = (e) => {
-  follower.style.top = (e.pageY + 40) + 'px';
-  follower.style.left = (e.pageX + 40) + 'px';
-}
+  const setFollower = (e) => {
+    follower.style.top = (e.pageY + 40) + 'px';
+    follower.style.left = (e.pageX + 40) + 'px';
+  }
+  document.addEventListener('mousemove', throttle(setFollower, 5));
+} catch (e) {}
 
 
 const throttle = (func, limit) => {
@@ -36,4 +37,26 @@ const throttle = (func, limit) => {
 }
 
 
-document.addEventListener('mousemove', throttle(setFollower, 5));
+
+const canvas = document.querySelector('.canvas')
+if (canvas) {
+
+  const ctx = canvas.getContext('2d')
+  ctx.beginPath();
+  ctx.moveTo(0, 680);
+  ctx.lineTo(100, 75);
+  ctx.lineTo(100, 25);
+  ctx.strokeStyle = '#09D323';
+  ctx.lineWidth = 2;
+  ctx.stroke();
+  
+  canvas.onclick = (e) => {
+    const rect = canvas.getBoundingClientRect()
+    const x = event.clientX - rect.left
+    const y = event.clientY - rect.top
+    ctx.lineTo(x, y);
+    ctx.stroke();
+
+    ctx.fillRect(x - 1, y - 1, 5, 5);
+  }
+}
